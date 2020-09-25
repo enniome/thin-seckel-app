9bfa16e5c832b2e79b976ed77301530b19d6d683
Ennio Meier <ennio.meier@adna.io>
2020-09-25 09:09:26 +0200

    Initial Commit

---
+++ thin-seckel/thin_seckelApp.swift
@@ -0,0 +1,17 @@
+//
+//  thin_seckelApp.swift
+//  thin-seckel
+//
+//  Created by Ennio Meier on 9/25/20.
+//
+
+import SwiftUI
+
+@main
+struct thin_seckelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
